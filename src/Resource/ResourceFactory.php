e7fd685b6a2d6ae039e3fbd46aedb95a18ed1f2c
WENI HSU <hsu.weni@gmail.com>
2019-10-28 00:13:28 +0800

    refactor #15

--- src/Resource/ResourceFactory.php
+++ src/Resource/ResourceFactory.php
@@ -4,7 +4,6 @@
 namespace App\Resource;
 
 
-use App\Middleware\CommonErrorMiddleware;
 use Exception;
 
 class ResourceFactory
@@ -15,22 +14,20 @@ class ResourceFactory
         $str = preg_replace_callback('/[_|-]([a-z])/', function ($matches) {
             return strtoupper($matches[1]);
         }, $resourceType);
-        return 'App\Resource\\' . ucfirst($str) . 'Resource';
+
+        $className = 'App\Resource\\' . ucfirst($str) . 'Resource';
+
+        if (class_exists($className)) {
+            return $className;
+        }
+
+        // 沒有符合的 className 時，這裡的 Exception 要自己處理
+        throw new Exception('URI not found.');
     }
 
     static public function get($resourceType)
     {
         $className = self::getClassName($resourceType);
-
-        if (class_exists($className)) {
-            return new $className();
-        }
-
-        // 沒有符合的 className 時，這裡的 Exception 要自己處理
-        throw new Exception('URI not found.');
-//        if (!class_exists($className)) {
-//            (new CommonErrorMiddleware($self))->run();
-//            exit;
-//        }
+        return new $className();
     }
 }
