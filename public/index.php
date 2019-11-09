b6905f051a1120d95870d03d41eb49bd89c160c7
WENI HSU <hsu.weni@gmail.com>
2019-11-09 12:49:00 +0800

    設定 error_reporting 偵錯模式 #24

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 
-ini_set('display_errors', 0);
+// 監聽捕獲的錯誤級別
+error_reporting(E_ALL);
+// 是否開啟錯誤資訊回顯 將錯誤輸出至標準輸出（瀏覽器/命令列）
+ini_set('display_errors', true);
+// 死否開啟錯誤日誌記錄 將錯誤記錄至 ini：error_log 指定檔案
+ini_set('log_errors', true);
+ini_set('error_log', __DIR__ . '/../data/php-errors.log');
 
 use App\Router\MyRouter;
 use Symfony\Component\Yaml\Yaml;
