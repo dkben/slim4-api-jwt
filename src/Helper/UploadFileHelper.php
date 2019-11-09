07a9d1b4f9c853a2c951983723e943953121649a
WENI HSU <hsu.weni@gmail.com>
2019-11-09 13:00:36 +0800

    調整參數位置 #7

--- src/Helper/UploadFileHelper.php
+++ src/Helper/UploadFileHelper.php
@@ -10,27 +10,29 @@ class UploadFileHelper
 {
     private $accept;
 
+    private $path;
+
     public function __construct()
     {
         $this->accept = ['jpg', 'jpeg', 'png'];
+
+        $this->path = $GLOBALS['systemConfig']['upload']['public'] . date('Ymd') . '/';
+
+        if (!file_exists($this->path)) {
+            mkdir($this->path, 0755, true);
+        }
     }
 
     public function upload()
     {
-        $path = $GLOBALS['systemConfig']['upload']['public'] . date('Ymd') . '/';
-
-        if (!file_exists($path)) {
-            mkdir($path, 0755, true);
-        }
-
         $basename = bin2hex(random_bytes(8));
-        $tmpName = $path . $basename;
+        $tmpName = $this->path . $basename;
         file_put_contents($tmpName, file_get_contents('php://input'));
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $mimeType = finfo_file($finfo, $tmpName);
         $mimes = new MimeTypes;
         $extension = $mimes->getExtension($mimeType);
-        $newName = $path . sprintf('%s.%0.8s', $basename, $extension);
+        $newName = $this->path . sprintf('%s.%0.8s', $basename, $extension);
 
         if (in_array($extension, $this->accept)) {
             rename($tmpName, $newName);
