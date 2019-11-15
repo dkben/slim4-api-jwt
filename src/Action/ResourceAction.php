47083baed2326964533f20718b6e73ce85457afc
WENI HSU <hsu.weni@gmail.com>
2019-11-15 09:14:22 +0800

    ResourceAction 改用 Singleton 模式，註解 #22

--- src/Action/ResourceAction.php
+++ src/Action/ResourceAction.php
@@ -9,6 +9,7 @@ use App\Router\BaseRouter;
 
 /**
  * 依照網址動態使用 \Resource\ 裡的 Class Method
+ * 因為這裡的 get, post, put, patch, delete 是獨立的入口點，所以都需要執行 create 方法
  * Class ResourceAction
  * @package App\Action
  */
@@ -25,10 +26,18 @@ class ResourceAction extends BaseAction
         return $this->resource;
     }
 
+    /**
+     * 獨立的入口點，參數也只能由這裡傳入，所以都需要執行 create 方法
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function get($request, $response, $args)
     {
-        $id = isset($args['id']) ? $args['id'] : null;
         $resource = $this->create($request, $response, $args);
+
+        $id = isset($args['id']) ? $args['id'] : null;
         if (is_string($resource)) {
             $data = $resource;
             $status = 400;
@@ -36,41 +45,74 @@ class ResourceAction extends BaseAction
             $data = $resource->get($id);
             $status = 200;
         }
+
         $response->getBody()->write($data);
         return BaseRouter::staticResponse($response, $status);
     }
 
+    /**
+     * 獨立的入口點，參數也只能由這裡傳入，所以都需要執行 create 方法
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function post($request, $response, $args)
     {
-        $data = json_decode($request->getBody()->getContents());
         $resource = $this->create($request, $response, $args);
+
+        $data = json_decode($request->getBody()->getContents());
         $response->getBody()->write($resource->post($data));
         return BaseRouter::staticResponse($response, 201);
     }
 
+    /**
+     * 獨立的入口點，參數也只能由這裡傳入，所以都需要執行 create 方法
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function put($request, $response, $args)
     {
+        $resource = $this->create($request, $response, $args);
+
         $id = isset($args['id']) ? $args['id'] : null;
         $data = json_decode($request->getBody()->getContents());
-        $resource = $this->create($request, $response, $args);
         $response->getBody()->write($resource->put($id, $data));
         return BaseRouter::staticResponse($response, 200);
     }
 
+    /**
+     * 獨立的入口點，參數也只能由這裡傳入，所以都需要執行 create 方法
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function patch($request, $response, $args)
     {
+        $resource = $this->create($request, $response, $args);
+
         $id = isset($args['id']) ? $args['id'] : null;
         $data = json_decode($request->getBody()->getContents());
-        $resource = $this->create($request, $response, $args);
         $response->getBody()->write($resource->patch($id, $data));
         return BaseRouter::staticResponse($response, 200);
     }
 
+    /**
+     * 獨立的入口點，參數也只能由這裡傳入，所以都需要執行 create 方法
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function delete($request, $response, $args)
     {
+        $resource = $this->create($request, $response, $args);
+
         $id = isset($args['id']) ? $args['id'] : null;
         $data = json_decode($request->getBody()->getContents());
-        $resource = $this->create($request, $response, $args);
         $response->getBody()->write($resource->delete($id, $data));
         return BaseRouter::staticResponse($response, 200);
     }
