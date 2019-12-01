7a82be9b28b0fc8936ef185d330a8abe6755ac03
WENI HSU <hsu.weni@gmail.com>
2019-12-01 20:19:15 +0800

    Entity Validate Message return json #5, #34

--- src/Exception/ExceptionResponse.php
+++ src/Exception/ExceptionResponse.php
@@ -8,7 +8,13 @@ class ExceptionResponse
 {
     static public function response($message, $code)
     {
-        $response = array('message' => $message, 'code' => $code);
+        // 目前 Entity Validate 需要使用 key: value 方式回傳，使用以下方式判別
+        if (is_null(json_decode($message))) {
+            $response = array('message' => $message, 'code' => $code);
+        } else {
+            $response = array('message' => json_decode($message), 'code' => $code);
+        }
+
         echo json_encode($response);
         header('Content-Type: application/json');
         http_response_code(400);
